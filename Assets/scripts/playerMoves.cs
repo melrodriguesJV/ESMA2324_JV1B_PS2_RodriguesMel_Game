eef8a4af0e789f749c091afb90dd135ebb1eefb6
melrodriguesJV <mel1985rod@gmail.com>
2024-05-15 16:18:19 +0200

    wall jump start

--- Assets/scripts/playerMoves.cs
+++ Assets/scripts/playerMoves.cs
@@ -6,14 +6,17 @@ public class playerMoves : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpforce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
-    private bool grounded;
+    private BoxCollider2D boxCollider;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -26,11 +29,11 @@ public class playerMoves : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(1,1,1);
 
-        if (Input.GetKey(KeyCode.Space) && grounded)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
             Jump();
 
         anim.SetBool("run", horizontalInput !=0);
-        anim.SetBool("grounded", grounded);
+        anim.SetBool("grounded", isGrounded());
 
     }
 
@@ -38,12 +41,22 @@ public class playerMoves : MonoBehaviour
     {
         body.velocity = new Vector2(body.velocity.x, jumpforce);
         anim.SetTrigger("jump");
-        grounded = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Ground")
-            grounded = true;
+        
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
     }
 }
