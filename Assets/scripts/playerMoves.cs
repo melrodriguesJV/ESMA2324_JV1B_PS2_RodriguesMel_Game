ec01dcb143e9d5b1a7bb856ad5d7e49830120612
melrodriguesJV <mel1985rod@gmail.com>
2024-05-15 14:42:13 +0200

    move left right

--- Assets/scripts/playerMoves.cs
+++ Assets/scripts/playerMoves.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class playerMoves : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+
+    private void Awake()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, body.velocity.y);
     }
 }
