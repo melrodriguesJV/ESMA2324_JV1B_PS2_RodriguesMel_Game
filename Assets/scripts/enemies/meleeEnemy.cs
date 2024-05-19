7066801368270db7c428166afbdbedbb056b678f
melrodriguesJV <mel1985rod@gmail.com>
2024-05-19 21:17:54 +0200

    start coding dog

--- Assets/scripts/enemies/meleeEnemy.cs
+++ Assets/scripts/enemies/meleeEnemy.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class meleeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Attack Parameters")]
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+    //references
+    private Animator anim;
+    private health playerHealth;
+
+    private void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private bool PlayerInSight()
     {
-        
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
+            0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<health>();
+
+        return hit.collider != null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        //if player is still in range damage him
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
     }
 }
