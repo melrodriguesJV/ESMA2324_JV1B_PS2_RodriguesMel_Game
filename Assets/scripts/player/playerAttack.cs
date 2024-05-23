3dcba57307160831ebb4b4a23b7a66b3cf685dfd
melrodriguesJV <mel1985rod@gmail.com>
2024-05-23 10:59:28 +0200

    cancel melee

--- Assets/scripts/player/playerAttack.cs
+++ Assets/scripts/player/playerAttack.cs
@@ -8,17 +8,8 @@ public class playerAttack : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] bullets;
-    [SerializeField] private float range;
-
-    [Header("Collider Parameters")]
-    [SerializeField] private float colliderDistance;
-    [SerializeField] private BoxCollider2D boxCollider;
-
-    [Header("Enemy Layer")]
-    [SerializeField] private LayerMask enemyLayer;
 
     private Animator anim;
-    private health enemyHealth;
     private playerMoves playerMoves;
     private batPowerup bat;
     private float cooldownTimer = Mathf.Infinity;
@@ -35,9 +26,6 @@ public class playerAttack : MonoBehaviour
         if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMoves.canAttack())
             Attack();
 
-        if (Input.GetKeyDown(KeyCode.R) && cooldownTimer > attackCooldown && playerMoves.canAttack())
-            MeleeAttack();
-
         cooldownTimer += Time.deltaTime;
     }
 
@@ -49,30 +37,6 @@ public class playerAttack : MonoBehaviour
         bullets[FindBullet()].GetComponent<projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
 
-    private void MeleeAttack()
-    {
-        if (EnemyInSight() && bat.pick == true)
-            enemyHealth.TakeDamage(2);
-    }
-
-    private bool EnemyInSight()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
-            0, Vector2.left, 0, enemyLayer);
-
-        if (hit.collider != null)
-            enemyHealth = hit.transform.GetComponent<health>();
-
-        return hit.collider != null;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
-            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
-    }
 
     private int FindBullet()
     {
