1f31c7c9451d069d9a5a4e87485170fec2a39cba
melrodriguesJV <mel1985rod@gmail.com>
2024-05-26 20:39:10 +0200

    teleporter fix

--- Assets/scripts/management/teleport.cs
+++ Assets/scripts/management/teleport.cs
@@ -5,14 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class teleport : MonoBehaviour
 {
+    public bool playerIsClose;
     public GameObject Player;
     public GameObject Health;
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
             SceneManager.LoadSceneAsync(2);
         }
 
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
     }
 }
