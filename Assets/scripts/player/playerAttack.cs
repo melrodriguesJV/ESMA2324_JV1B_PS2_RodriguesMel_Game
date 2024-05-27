6dae62860be65ff1179371f440d67361cf6749e2
melrodriguesJV <mel1985rod@gmail.com>
2024-05-27 13:48:21 +0200

    melee attack restart

--- Assets/scripts/player/playerAttack.cs
+++ Assets/scripts/player/playerAttack.cs
@@ -8,10 +8,14 @@ public class playerAttack : MonoBehaviour
     [SerializeField] private float attackCooldown;
     [SerializeField] private Transform firePoint;
     [SerializeField] private GameObject[] bullets;
+    [SerializeField] private Transform meleePoint;
+    [SerializeField] private float meleeRange;
+    [SerializeField] private LayerMask enemyLayer;
 
     private Animator anim;
     private playerMoves playerMoves;
     private batPowerup bat;
+    private health hp;
     private float cooldownTimer = Mathf.Infinity;
 
     private void Awake()
@@ -19,12 +23,16 @@ public class playerAttack : MonoBehaviour
         anim = GetComponent<Animator>();
         playerMoves = GetComponent<playerMoves>();
         bat = GetComponent<batPowerup>();
+        hp = GetComponent<health>();
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMoves.canAttack())
             Attack();
+
+        if (Input.GetKeyDown(KeyCode.B) && cooldownTimer > attackCooldown && playerMoves.canAttack())
+            Melee();
 
         cooldownTimer += Time.deltaTime;
     }
@@ -37,6 +45,18 @@ public class playerAttack : MonoBehaviour
         bullets[FindBullet()].GetComponent<projectile>().SetDirection(Mathf.Sign(transform.localScale.x));
     }
 
+    private void Melee()
+    {
+        if (bat.pick == true)
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(meleePoint.position, meleeRange, enemyLayer);
+
+            foreach(Collider2D enemy in hitEnemies)
+            {
+                hp.TakeDamage(1);
+            }
+        }
+    }
 
     private int FindBullet()
     {
