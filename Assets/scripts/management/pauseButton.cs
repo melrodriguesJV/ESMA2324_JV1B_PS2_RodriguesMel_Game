1d1ab89f1de8cab614ad726958215e7f98d6050d
melrodriguesJV <mel1985rod@gmail.com>
2024-05-29 16:27:22 +0200

    pause code update

--- Assets/scripts/management/pauseButton.cs
+++ Assets/scripts/management/pauseButton.cs
@@ -1,12 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pauseButton : MonoBehaviour
 {
     [SerializeField] private GameObject leaveButton;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject currentCamera;
+    [SerializeField] private GameObject canvas;
+    private PointManager pointManager;
     public void Pause()
     {
         leaveButton.SetActive(true);
+    }
+
+    public void Leave()
+    {
+        SceneManager.LoadSceneAsync(0);
+        leaveButton.SetActive(false);
+        Destroy(player);
+        Destroy(currentCamera);
+        Destroy(canvas);
+        pointManager.ChangePoints(0);
     }
 }
