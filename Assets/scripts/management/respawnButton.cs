c19bea3a949baa1c533163af8b712c79661e63aa
melrodriguesJV <mel1985rod@gmail.com>
2024-05-29 09:05:38 +0200

    working respawn

--- Assets/scripts/management/respawnButton.cs
+++ Assets/scripts/management/respawnButton.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class respawnButton : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject replayButton;
+    public void Replay()
+    {
+        SceneManager.LoadSceneAsync(1);
+        replayButton.SetActive(false);
+    }
 }
