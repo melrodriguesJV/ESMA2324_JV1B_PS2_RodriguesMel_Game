d516873c379335791686cd76697de34a153cc466
melrodriguesJV <mel1985rod@gmail.com>
2024-06-04 10:02:18 +0200

    sprite tag wall changing

--- Assets/scripts/tagWall.cs
+++ Assets/scripts/tagWall.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class tagWall : MonoBehaviour
 {
     [SerializeField] private int value;
+    [SerializeField] private Sprite covered;
     public bool playerIsClose;
     private SpriteRenderer spriteRend;
     private PointManager pointManager;
@@ -25,7 +26,7 @@ public class tagWall : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && playerIsClose && check == false)
         {
-            spriteRend.color = Color.red; // change l'apparence du mur
+            spriteRend.sprite = covered; // change l'apparence du mur
             pointManager.ChangePoints(value); // ajoute des points au score
             check = true;
         }
