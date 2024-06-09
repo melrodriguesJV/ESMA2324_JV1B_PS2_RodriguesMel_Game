8ed41ba0773d72f432c80dffd06c338f98cadd04
melrodriguesJV <mel1985rod@gmail.com>
2024-06-09 19:03:09 +0200

    final

--- Assets/scripts/management/Deadzone.cs
+++ Assets/scripts/management/Deadzone.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class Deadzone : MonoBehaviour
 {
     [SerializeField] private int damage;
     private health playerHealth;
+    private bool zoned;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Start()
     {
-        if (other.CompareTag("Player"))
+        zoned = false;
+        playerHealth = GetComponent<health>();
+    }
+
+    private void Update()
+    {
+        if (zoned)
         {
             playerHealth.TakeDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            zoned = true;
         }
     }
 }
